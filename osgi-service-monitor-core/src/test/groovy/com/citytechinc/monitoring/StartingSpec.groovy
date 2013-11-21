dcb99ece8aa56075899dcf11fb89db9e7d7fd87b
Josh Durbin <durbinjo593@gmail.com>
2013-11-21 10:05:43 -0800

    Added first test.

---
+++ osgi-service-monitor-core/src/test/groovy/com/citytechinc/monitoring/StartingSpec.groovy
@@ -0,0 +1,12 @@
+package com.citytechinc.monitoring
+
+import spock.lang.Specification
+
+class StartingSpec extends Specification {
+
+    def "Run me"() {
+
+        expect:
+        'blue' == 'blue'
+    }
+}
