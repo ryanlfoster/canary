f7460ee056ac81926365eeb3fd9d9d0a63adef86
Josh Durbin <durbinjo593@gmail.com>
2014-02-14 13:32:46 -0800

    Added test stubs

---
+++ src/test/groovy/com/citytechinc/canary/RecordHolderSpecification.groovy
@@ -0,0 +1,51 @@
+package com.citytechinc.canary
+
+import com.citytechinc.canary.api.monitor.AlarmCriteria
+import com.citytechinc.canary.api.monitor.DetailedPollResponse
+import com.citytechinc.canary.api.monitor.MonitoredServiceWrapper
+import com.citytechinc.canary.api.monitor.PollResponseType
+import com.citytechinc.canary.api.monitor.RecordHolder
+import spock.lang.Specification
+
+class RecordHolderSpecification extends Specification {
+
+    def "Test total failure rate, after alarmed, after reset"() {
+
+    }
+
+    def "Test unexcused failure rate, after alarmed, after reset"() {
+
+    }
+
+    def "Test total failure rate"() {
+
+    }
+
+    def "Test average execution time, after alarmed, after reset"() {
+
+    }
+
+    def "Test unexcused average execution time, after alarmed, after reset"() {
+
+    }
+
+    def "Test average execution time"() {
+
+    }
+
+    def "Test number of records, unexcused records, most recent poll date and status, number of polls, number of failures"() {
+
+    }
+
+    def "Test average failure rate alarm criteria alarmed after threshold"() {
+
+    }
+
+    def "Test average execution time alarm criteria alarmed after threshold"() {
+
+    }
+
+    def "Test recent polls alarm criteria alarmed after threshold"() {
+
+    }
+}
