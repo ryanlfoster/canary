d853e41e151fee1a80fb17dd788c0edbf8e4e8a7
Josh Durbin <durbinjo593@gmail.com>
2014-02-21 12:01:59 -0800

    Added name

--- src/main/groovy/com/citytechinc/canary/api/monitor/MonitoredServiceWrapper.groovy
+++ src/main/groovy/com/citytechinc/canary/api/monitor/MonitoredServiceWrapper.groovy
@@ -1,6 +1,7 @@
 package com.citytechinc.canary.api.monitor
 
 import groovy.transform.EqualsAndHashCode
+import org.apache.commons.lang.StringUtils
 
 /**
  *
@@ -14,6 +15,7 @@ public final class MonitoredServiceWrapper {
 
     @Delegate final MonitoredService monitor
     final String identifier
+    final String name
     final MonitoredServiceDefinition definition
     final AutomaticResetMonitor automaticResetMonitorDefinition
 
@@ -21,6 +23,7 @@ public final class MonitoredServiceWrapper {
 
         this.monitor = monitor
         identifier = monitor.class.canonicalName
+        name = StringUtils.substringAfterLast(identifier, '.').replaceAll('(.)([A-Z])', '$1 $2')
 
         definition = monitor.class.getAnnotation(MonitoredServiceDefinition)
         automaticResetMonitorDefinition = monitor.class.getAnnotation(AutomaticResetMonitor)
