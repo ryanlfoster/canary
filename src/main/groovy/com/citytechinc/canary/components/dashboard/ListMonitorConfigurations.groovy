e12b6a0fb24bff608bfc08f54d8f3cfcdaeace24
Josh Durbin <durbinjo593@gmail.com>
2014-03-03 17:40:21 -0800

    Added comment

--- src/main/groovy/com/citytechinc/canary/components/dashboard/ListMonitorConfigurations.groovy
+++ src/main/groovy/com/citytechinc/canary/components/dashboard/ListMonitorConfigurations.groovy
@@ -7,6 +7,13 @@ import org.apache.felix.scr.annotations.sling.SlingServlet
 import org.apache.sling.api.SlingHttpServletRequest
 import org.apache.sling.api.SlingHttpServletResponse
 
+/**
+ *
+ * @author Josh Durbin, CITYTECH, Inc. 2014
+ *
+ * Copyright 2014 CITYTECH, Inc.
+ *
+ */
 @SlingServlet(
         resourceTypes = 'canary/components/page/dashboard',
         methods = 'GET',
