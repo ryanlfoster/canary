a944832c0ed6326d460bc2f81e61e826a7f08a90
Michael Paull <mpaull@citytechinc.com>
2014-04-08 20:33:27 -0400

    removed config.js and moved its contents to application init and chart view config.

--- src/main/content/jcr_root/apps/canary/components/page/dashboard/clientlibs/canary-dashboard/js/views/chart.js
+++ src/main/content/jcr_root/apps/canary/components/page/dashboard/clientlibs/canary-dashboard/js/views/chart.js
@@ -2,14 +2,72 @@
 
   'use strict';
 
+
+  /** This object holds the configration for the ChartJS output. */
+  var chartJSConfig = {
+
+    scaleOverlay : false, //Boolean - If we show the scale above the chart data
+
+    scaleOverride : false, //Boolean - If we want to override with a hard coded scale
+
+    //** Required if scaleOverride is true **
+    scaleSteps : null, //Number - The number of steps in a hard coded scale
+    scaleStepWidth : null, //Number - The value jump in the hard coded scale
+    scaleStartValue : null, //Number - The scale starting value
+
+    scaleLineColor : "rgba(0,0,0,.1)", //String - Colour of the scale line
+
+    scaleLineWidth : 1, //Number - Pixel width of the scale line
+
+    scaleShowLabels : true, //Boolean - Whether to show labels on the scale
+
+    scaleLabel : "<%=value%>", //Interpolated JS string - can access value
+
+    scaleFontFamily : "'Arial'", //String - Scale label font declaration for the scale label
+
+    scaleFontSize : 12, //Number - Scale label font size in pixels
+
+    scaleFontStyle : "normal", //String - Scale label font weight style
+
+    scaleFontColor : "#666", //String - Scale label font colour
+
+    scaleShowGridLines : true, //Boolean - Whether grid lines are shown across the chart
+
+    scaleGridLineColor : "rgba(0,0,0,.05)", //String - Colour of the grid lines
+
+    scaleGridLineWidth : 1, //Number - Width of the grid lines
+
+    bezierCurve : true, //Boolean - Whether the line is curved between points
+
+    pointDot : true, //Boolean - Whether to show a dot for each point
+
+    pointDotRadius : 3, //Number - Radius of each point dot in pixels
+
+    pointDotStrokeWidth : 1, //Number - Pixel width of point dot stroke
+
+    datasetStroke : true, //Boolean - Whether to show a stroke for datasets
+
+    datasetStrokeWidth : 2, //Number - Pixel width of dataset stroke
+
+    datasetFill : true, //Boolean - Whether to fill the dataset with a colour
+
+    animation : true, //Boolean - Whether to animate the chart
+
+    animationSteps : 60, //Number - Number of animation steps
+
+    animationEasing : "easeOutQuart", //String - Animation easing effect
+
+    onAnimationComplete : null //Function - Fires when the animation is complete
+
+  };
+
+
+
+
   Canary.ChartView = Ember.View.extend({
 
     didInsertElement: function() {
-      // Need to grab a calculated value or that value will never get calculated
-      // and observers will never fire.
-      this.get('controller.records.length');
-
-      // Avoid asynchronicity issues by calling this once directly.
+      /** Avoid asynchronicity issues by calling this once directly. */
       this.drawLineChart();
     },
 
@@ -17,11 +75,13 @@
 
     hideLabels: false,
 
-    // Methods:
-
-    // Draw line chart
+    /** Draw line chart */
     lineChartBuilder: function() {
 
+      /** Cache a reference the jQuery object representing the chart container. */
+      var $chartContainer = this.$(".chart");
+
+      /** Build the chart data object without labels and data points. */
       var chartData = {labels: [], datasets: [{
         fillColor : "rgba(151,187,205,0.5)",
         strokeColor : "rgba(151,187,205,1)",
@@ -30,6 +90,7 @@
         data: []
       }]};
 
+      /** If there are records, loop over them and put the labels and datapoints into the chart data object. */
       if ( this.get('controller.records.records').length > 0 ) {
         _.forEach(this.get('controller.records.records'), function(record) {
           chartData.labels.push(record.startTime);
@@ -37,19 +98,17 @@
         });
       }
 
-      // Cache a reference the jQuery object representing the chart container.
-      var $chartContainer = this.$(".chart");
-
+      /** If there is a jQuery onject to put the output in and some data in the chart data object ... */
       if ($chartContainer && chartData.labels.length > 0) {
 
-        // Set the width and the height of the chart container to support responsive layout.
+        /** Set the width and the height of the chart container to support responsive layout. */
         $chartContainer.attr('width', $chartContainer.parent().width());
         $chartContainer.attr('height', $chartContainer.parent().width() / 1.6);
 
-        // Set the getContext value for the canvas element.
+        /** Set the getContext value for the canvas element. */
         var chartContext = $chartContainer.get(0).getContext("2d");
 
-        // Hide the labels if the controller says to.
+        /** Hide the labels if the controller says to. */
         if ( this.get( 'hideLabels' ) === true ) {
           var numLabels = chartData.labels.length;
           var newLabels = [];
@@ -59,17 +118,19 @@
           chartData.labels = newLabels;
         }
 
-        // Create a new Line Chart using Chart.js.
-        Canary.recordChart = new Chart(chartContext).Line(chartData, Canary.config.chartJSLineChartOptions);
+        /** Create a new Line Chart using Chart.js. */
+        Canary.recordChart = new Chart(chartContext).Line(chartData, chartJSConfig);
 
       }
 
     },
 
-    // This method observes another observer of the chart data.
-    // Observing the chart data directly doesn't seem to work.
-    // Debouncing the call to the actual chartbuilder method,
-    // because this observer fires for each datapoint.
+    /**
+     * Observes another observer of the chart data.
+     * Observing the chart data directly doesn't seem to work.
+     * Debouncing the call to the actual chartbuilder method,
+     * because this observer fires for each datapoint.
+     */
     drawLineChart: function() {
       Ember.run.debounce(this, this.lineChartBuilder, 100);
     }.observes('controller.average')
