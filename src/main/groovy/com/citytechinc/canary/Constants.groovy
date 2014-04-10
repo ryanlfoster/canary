a8cdb4992231e77099653abd08b15906bfd75f17
Josh Durbin <durbinjo593@gmail.com>
2014-04-09 21:57:04 -0500

    Adding response monitors, updated constants

--- src/main/groovy/com/citytechinc/canary/Constants.groovy
+++ src/main/groovy/com/citytechinc/canary/Constants.groovy
@@ -13,27 +13,27 @@ import org.joda.time.format.DateTimeFormatter
  */
 public final class Constants {
 
-    static final CITYTECH_SERVICE_VENDOR_NAME = 'CITYTECH, Inc.'
+    public static final CITYTECH_SERVICE_VENDOR_NAME = 'CITYTECH, Inc.'
 
     // COMMON DATES
-    static final String JMX_DATE_TIME_DEFINITION = 'yyyy-MM-dd HH:mm:ss'
-    static final FastDateFormat JMX_DATE_TIME_FORMATTER = FastDateFormat.getInstance(JMX_DATE_TIME_DEFINITION)
-    static final DateTimeFormatter JMX_DATE_TIME_PARSER = DateTimeFormat.forPattern(JMX_DATE_TIME_DEFINITION)
+    public static final String JMX_DATE_TIME_DEFINITION = 'yyyy-MM-dd HH:mm:ss'
+    public static final FastDateFormat JMX_DATE_TIME_FORMATTER = FastDateFormat.getInstance(JMX_DATE_TIME_DEFINITION)
+    public static final DateTimeFormatter JMX_DATE_TIME_PARSER = DateTimeFormat.forPattern(JMX_DATE_TIME_DEFINITION)
 
     // JCR PERSISTENCE
-    static final String JCR_POLL_RESPONSE_NODE_STORAGE_DEFINITION = 'yyyy-MM-dd-HH-mm-ss-SSS'
-    static final FastDateFormat JCR_POLL_RESPONSE_NODE_STORAGE_FORMATTER = FastDateFormat.getInstance(JCR_POLL_RESPONSE_NODE_STORAGE_DEFINITION)
+    public static final String JCR_POLL_RESPONSE_NODE_STORAGE_DEFINITION = 'yyyy-MM-dd-HH-mm-ss-SSS'
+    public static final FastDateFormat JCR_POLL_RESPONSE_NODE_STORAGE_FORMATTER = FastDateFormat.getInstance(JCR_POLL_RESPONSE_NODE_STORAGE_DEFINITION)
 
-    static final String JCR_PERSISTENCE_STORAGE_ROOT_NODE = 'service_monitoring'
-    static final String JCR_PERSISTENCE_STORAGE_ROOT_NODE_PATH = '/etc/' + JCR_PERSISTENCE_STORAGE_ROOT_NODE
+    public static final String JCR_PERSISTENCE_STORAGE_ROOT_NODE = 'service_monitoring'
+    public static final String JCR_PERSISTENCE_STORAGE_ROOT_NODE_PATH = '/etc/' + JCR_PERSISTENCE_STORAGE_ROOT_NODE
 
     // NODE IDENTIFIERS
-    static final String JCR_NODE_TYPE_MONITORED_SERVICE_STORAGE = 'canary:MonitorServiceStorage'
-    static final String JCR_NODE_TYPE_RECORD_HOLDER = 'canary:MonitorRecords'
-    static final String JCR_NODE_TYPE_DETAILED_POLL_RESPONSE = 'canary:PollResult'
+    public static final String JCR_NODE_TYPE_MONITORED_SERVICE_STORAGE = 'canary:MonitorServiceStorage'
+    public static final String JCR_NODE_TYPE_RECORD_HOLDER = 'canary:MonitorRecords'
+    public static final String JCR_NODE_TYPE_DETAILED_POLL_RESPONSE = 'canary:PollResult'
 
     // ABSTRACT JSON RESPONSE SERVLET
-    static final String ABSTRACT_JSON_RESPONSE_SERVLET_CONTENT_TYPE = 'application/json'
-    static final String ABSTRACT_JSON_RESPONSE_SERVLET_CHARACTER_ENCODING = 'utf-8'
-    static final String ABSTRACT_JSON_RESPONSE_SERVLET_DEFAULT_DATE_DEFINITION = 'yyyy-MM-dd\'T\'HH:mm:ss.SSSZ'
+    public static final String ABSTRACT_JSON_RESPONSE_SERVLET_CONTENT_TYPE = 'application/json'
+    public static final String ABSTRACT_JSON_RESPONSE_SERVLET_CHARACTER_ENCODING = 'utf-8'
+    public static final String ABSTRACT_JSON_RESPONSE_SERVLET_DEFAULT_DATE_DEFINITION = 'yyyy-MM-dd\'T\'HH:mm:ss.SSSZ'
 }
