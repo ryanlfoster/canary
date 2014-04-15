56bae1f7b30c9f5b9c22babeea947262a6d5d4c9
Josh Durbin <durbinjo593@gmail.com>
2014-04-14 23:27:34 -0500

    Added sling catch all performance monitor that inspects all requests not matching the other two explicit monitors (dynamic and /content page requests). Added inactive bundle and component monitors Added configs for inactive bundles and components monitor in addition to the sling catch all performance monitor

---
+++ src/main/groovy/com/citytechinc/canary/services/monitor/InactiveBundlesMonitor.groovy
@@ -0,0 +1,37 @@
+package com.citytechinc.canary.services.monitor
+
+import com.citytechinc.canary.Constants
+import com.citytechinc.canary.api.monitor.AutomaticResetMonitor
+import com.citytechinc.canary.api.monitor.MonitoredService
+import com.citytechinc.canary.api.monitor.MonitoredServiceDefinition
+import com.citytechinc.canary.api.monitor.PollResponse
+import org.apache.felix.scr.annotations.Component
+import org.apache.felix.scr.annotations.ConfigurationPolicy
+import org.apache.felix.scr.annotations.Property
+import org.apache.felix.scr.annotations.Properties
+import org.apache.felix.scr.annotations.Service
+import org.osgi.framework.Constants as OsgiConstants
+
+import java.util.concurrent.TimeUnit
+
+/**
+ *
+ * @author Josh Durbin, CITYTECH, Inc. 2014
+ *
+ * Copyright 2014 CITYTECH, Inc.
+ *
+ */
+@Component(policy = ConfigurationPolicy.REQUIRE, immediate = true)
+@Service
+@Properties(value = [
+        @Property(name = OsgiConstants.SERVICE_VENDOR, value = Constants.CITYTECH_SERVICE_VENDOR_NAME) ])
+@MonitoredServiceDefinition(description = 'Checks for inactive bundles', pollInterval = 30, pollIntervalUnit = TimeUnit.SECONDS, alarmThreshold = 10)
+@AutomaticResetMonitor(resetInterval = 30, resetIntervalUnit = TimeUnit.MINUTES)
+class InactiveBundlesMonitor implements MonitoredService {
+
+    @Override
+    PollResponse poll() {
+
+        PollResponse.SUCCESS()
+    }
+}
---
+++ src/main/groovy/com/citytechinc/canary/services/monitor/InactiveComponentsMonitor.groovy
@@ -0,0 +1,63 @@
+package com.citytechinc.canary.services.monitor
+
+import com.citytechinc.canary.Constants
+import com.citytechinc.canary.api.monitor.AutomaticResetMonitor
+import com.citytechinc.canary.api.monitor.MonitoredService
+import com.citytechinc.canary.api.monitor.MonitoredServiceDefinition
+import com.citytechinc.canary.api.monitor.PollResponse
+import org.apache.felix.scr.Component
+import org.apache.felix.scr.ScrService
+import org.apache.felix.scr.annotations.Component as ComponentAnnotation
+import org.apache.felix.scr.annotations.ConfigurationPolicy
+import org.apache.felix.scr.annotations.Property
+import org.apache.felix.scr.annotations.Properties
+import org.apache.felix.scr.annotations.Reference
+import org.apache.felix.scr.annotations.Service
+import org.osgi.framework.Constants as OsgiConstants
+import org.osgi.service.cm.ConfigurationAdmin
+
+import java.util.concurrent.TimeUnit
+
+/**
+ *
+ * @author Josh Durbin, CITYTECH, Inc. 2014
+ *
+ * Copyright 2014 CITYTECH, Inc.
+ *
+ */
+@ComponentAnnotation(policy = ConfigurationPolicy.REQUIRE, immediate = true)
+@Service
+@Properties(value = [
+        @Property(name = OsgiConstants.SERVICE_VENDOR, value = Constants.CITYTECH_SERVICE_VENDOR_NAME) ])
+@MonitoredServiceDefinition(description = 'Checks for inactive components', pollInterval = 10, pollIntervalUnit = TimeUnit.SECONDS, alarmThreshold = 12)
+@AutomaticResetMonitor(resetInterval = 10, resetIntervalUnit = TimeUnit.SECONDS)
+class InactiveComponentsMonitor implements MonitoredService {
+
+    @Reference
+    ScrService scrService
+
+    @Reference
+    ConfigurationAdmin configurationAdmin
+
+    @Override
+    PollResponse poll() {
+
+        def messages = []
+
+        scrService.components.findAll {
+            it.state == Component.STATE_DISABLED
+        }.each {
+            messages.add("The service ${it.id} is disabled.")
+        }
+
+        scrService.components.findAll {
+            it.state == Component.STATE_UNSATISFIED
+        }.findAll {
+            configurationAdmin.getConfiguration(it.configurationPid)
+        }.each {
+            messages.add("The service ${it.id} requires a configuration ${it.configurationPid} and none exists.")
+        }
+
+        messages ? PollResponse.ERROR().addMessages(messages.collect { it.toString() }) : PollResponse.SUCCESS()
+    }
+}
