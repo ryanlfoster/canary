675a1c20712da71aaa163b215dcd0257c54e1674
Josh Durbin <durbinjo593@gmail.com>
2014-04-16 16:53:07 -0700

    Updated monitors and configs...

--- src/main/groovy/com/citytechinc/canary/services/monitor/statemonitoring/InactiveBundlesMonitor.groovy
+++ src/main/groovy/com/citytechinc/canary/services/monitor/statemonitoring/InactiveBundlesMonitor.groovy
@@ -5,12 +5,20 @@ import com.citytechinc.canary.api.monitor.AutomaticResetMonitor
 import com.citytechinc.canary.api.monitor.MonitoredService
 import com.citytechinc.canary.api.monitor.MonitoredServiceDefinition
 import com.citytechinc.canary.api.monitor.PollResponse
+import groovy.util.logging.Slf4j
+import org.apache.felix.scr.annotations.Activate
 import org.apache.felix.scr.annotations.Component
 import org.apache.felix.scr.annotations.ConfigurationPolicy
+import org.apache.felix.scr.annotations.Modified
 import org.apache.felix.scr.annotations.Property
 import org.apache.felix.scr.annotations.Properties
+import org.apache.felix.scr.annotations.PropertyUnbounded
 import org.apache.felix.scr.annotations.Service
+import org.apache.sling.commons.osgi.PropertiesUtil
+import org.osgi.framework.Bundle
+import org.osgi.framework.BundleContext
 import org.osgi.framework.Constants as OsgiConstants
+import org.osgi.service.component.ComponentContext
 
 import java.util.concurrent.TimeUnit
 
@@ -27,11 +35,40 @@ import java.util.concurrent.TimeUnit
         @Property(name = OsgiConstants.SERVICE_VENDOR, value = Constants.CITYTECH_SERVICE_VENDOR_NAME) ])
 @MonitoredServiceDefinition(description = 'Checks for inactive bundles', pollInterval = 30, pollIntervalUnit = TimeUnit.SECONDS, alarmThreshold = 10)
 @AutomaticResetMonitor(resetInterval = 30, resetIntervalUnit = TimeUnit.MINUTES)
+@Slf4j
 class InactiveBundlesMonitor implements MonitoredService {
+
+    @Property(name = 'ignoredBundles', label = 'Ignored Bundles', description = 'List of bundle symbolic names to ignore', unbounded = PropertyUnbounded.ARRAY)
+    private List<String> ignoredBundles
+
+    BundleContext bundleContext
+
+    @Activate
+    @Modified
+    protected void activate(ComponentContext componentContext) throws Exception {
+
+        bundleContext = componentContext.bundleContext
+        ignoredBundles = PropertiesUtil.toStringArray(componentContext.properties.get('ignoredBundles')) as List
+    }
 
     @Override
     PollResponse poll() {
 
-        PollResponse.SUCCESS()
+        log.debug("Polling...")
+
+        def messages = bundleContext.bundles.findAll {
+
+            (!ignoredBundles.contains(it.symbolicName) &&
+
+                    (it.state != Bundle.ACTIVE
+
+                            || !(it.state == Bundle.STARTING && it.headers.get(OsgiConstants.BUNDLE_ACTIVATIONPOLICY).equals(OsgiConstants.ACTIVATION_LAZY))
+                            || !it.headers.get(OsgiConstants.FRAGMENT_HOST)))
+        }.collect {
+
+            "The bundle ${it.symbolicName} is not in a healthy state".toString()
+        }
+
+        messages ? PollResponse.ERROR().addMessages(messages) : PollResponse.SUCCESS()
     }
 }
--- src/main/groovy/com/citytechinc/canary/services/monitor/statemonitoring/InactiveComponentsMonitor.groovy
+++ src/main/groovy/com/citytechinc/canary/services/monitor/statemonitoring/InactiveComponentsMonitor.groovy
@@ -5,14 +5,19 @@ import com.citytechinc.canary.api.monitor.AutomaticResetMonitor
 import com.citytechinc.canary.api.monitor.MonitoredService
 import com.citytechinc.canary.api.monitor.MonitoredServiceDefinition
 import com.citytechinc.canary.api.monitor.PollResponse
+import groovy.util.logging.Slf4j
 import org.apache.felix.scr.Component
 import org.apache.felix.scr.ScrService
+import org.apache.felix.scr.annotations.Activate
 import org.apache.felix.scr.annotations.Component as ComponentAnnotation
 import org.apache.felix.scr.annotations.ConfigurationPolicy
+import org.apache.felix.scr.annotations.Modified
 import org.apache.felix.scr.annotations.Property
 import org.apache.felix.scr.annotations.Properties
+import org.apache.felix.scr.annotations.PropertyUnbounded
 import org.apache.felix.scr.annotations.Reference
 import org.apache.felix.scr.annotations.Service
+import org.apache.sling.commons.osgi.PropertiesUtil
 import org.osgi.framework.Constants as OsgiConstants
 import org.osgi.service.cm.ConfigurationAdmin
 
@@ -29,9 +34,13 @@ import java.util.concurrent.TimeUnit
 @Service
 @Properties(value = [
         @Property(name = OsgiConstants.SERVICE_VENDOR, value = Constants.CITYTECH_SERVICE_VENDOR_NAME) ])
-@MonitoredServiceDefinition(description = 'Checks for inactive components', pollInterval = 10, pollIntervalUnit = TimeUnit.SECONDS, alarmThreshold = 12)
-@AutomaticResetMonitor(resetInterval = 10, resetIntervalUnit = TimeUnit.SECONDS)
+@MonitoredServiceDefinition(description = 'Checks for inactive components', pollInterval = 30, pollIntervalUnit = TimeUnit.SECONDS, alarmThreshold = 12)
+@AutomaticResetMonitor(resetInterval = 30, resetIntervalUnit = TimeUnit.SECONDS)
+@Slf4j
 class InactiveComponentsMonitor implements MonitoredService {
+
+    @Property(name = 'ignoredComponents', label = 'Ignored components', description = 'List of component ids to ignore', unbounded = PropertyUnbounded.ARRAY)
+    private List<String> ignoredComponents
 
     @Reference
     ScrService scrService
@@ -39,18 +48,30 @@ class InactiveComponentsMonitor implements MonitoredService {
     @Reference
     ConfigurationAdmin configurationAdmin
 
+    @Activate
+    @Modified
+    protected void activate(Map<String, Object> properties) throws Exception {
+
+        ignoredComponents = PropertiesUtil.toStringArray(properties.get('ignoredComponents')) as List
+    }
+
     @Override
     PollResponse poll() {
 
-        def messages = []
+        log.debug("Polling...")
 
-        scrService.components.findAll {
+        def messages = []
+        def nonExemptComponents = scrService.components.findAll {
+            !ignoredComponents.contains(it.id)
+        }
+
+        nonExemptComponents.findAll {
             it.state == Component.STATE_DISABLED
         }.each {
             messages.add("The service ${it.id} is disabled.")
         }
 
-        scrService.components.findAll {
+        nonExemptComponents.findAll {
             it.state == Component.STATE_UNSATISFIED
         }.findAll {
             configurationAdmin.getConfiguration(it.configurationPid)
